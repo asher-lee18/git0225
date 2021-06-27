6d1966c07bbe081fbf2a20f49f5d5c004f8ab0ca
LiAsher <Asher_2020@163.com>
2021-06-27 15:14:02 +0800

    mastercommit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println("aaa");
+        System.out.println("master");
+
     }
 }
